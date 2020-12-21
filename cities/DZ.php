dd58020556f6966646b30bfaae7a7476ecc4031a
embarekmessai <m_mebarek@hotmail.com>
2020-12-21 22:49:00 +0100

    Create cities file

---
+++ cities/DZ.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * ALgerian Cities
+ * @author   Embarek MESSAI <m_mebarek@hotmail.com>
+ * @version  1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
---
+++ states/DZ.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * ALgerian States
+ * @author   Embarek MESSAI <m_mebarek@hotmail.com>
+ * @version  1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+ 
